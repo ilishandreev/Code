cdb198886aec138d7c6e58e0784ef35c75702c6e
Илья Андреев <ilish@iMac-Ila.local>
2021-10-11 15:05:47 +0300

    js

--- js/script.js
+++ js/script.js
@@ -1,81 +1,53 @@
-$(document).ready(function(){
-	$('.cover').tilt({
-scale: 1.05
-	})
-})
+// $(document).ready(function(){
+// 	$('.cover').tilt({
+// scale: 1.05
+// 	})
+// })
 
-var header = $('.header-mob'),
-scrollPrev = 0;
+// var header = $('.header-mob'),
+// scrollPrev = 0;
 
-$(window).scroll(function() {
-	var scrolled = $(window).scrollTop();
+// $(window).scroll(function() {
+// 	var scrolled = $(window).scrollTop();
 
-	if ( scrolled > 100 && scrolled > scrollPrev ) {
-		header.addClass('out');
-	} else {
-		header.removeClass('out');
-	}
-	scrollPrev = scrolled;
-})
+// 	if ( scrolled > 100 && scrolled > scrollPrev ) {
+// 		header.addClass('out');
+// 	} else {
+// 		header.removeClass('out');
+// 	}
+// 	scrollPrev = scrolled;
+// })
 
 //Ниже скрипт для открытия мобильно меню//
 
-const initDropdownMenu = (parent) => {
-	const items = parent.querySelectorAll('[data-dropdown-menu="item"]');
-	const btn = parent.querySelector('[data-dropdown-menu="btn"]');
-	const btnParent = btn.closest('.main-nav__item');
-	let isMove = true;
-  
-	const isMosueLeave = (evt) => {
-	  let isParent = evt.target.closest('[data-dropdown-menu="parent"]');
-	  isMove = isParent ? false : true;
-	};
-  
-	const parentMoseLeaveHandler = () => {
-	  if (isMove) {
-		closeMenu();
-		document.removeEventListener('mousemove', isMosueLeave);
-		parent.removeEventListener('mouseleave', debounce(parentMoseLeaveHandler));
-	  }
-	};
-  
-	const closeMenu = () => {
-	  if (items.length >= 6) {
-		items.forEach((item, index) => {
-		  if (index >= 6) {
-			item.classList.add('hide');
-		  }
-		});
-  
-		parent.classList.remove('open');
-		btnParent.classList.remove('hide');
-		document.removeEventListener('mousemove', isMosueLeave);
-	  }
-	};
-  
-	closeMenu();
-  
-	btn.addEventListener('click', () => {
-	  items.forEach((item) => {
-		if (item.classList.contains('hide')) {
-		  item.classList.remove('hide');
+const initMenu = () => {
+
+  const parent = document.querySelector('[data-dropdown-menu="parent"]')
+	if (parent) {
+		const btn = parent.querySelector('[data-dropdown-menu="btn"]')
+		const menu = parent.querySelector('[data-dropdown-menu="menu"]')
+		
+		const openMenu = () => {
+			btn.classList.toggle('open')
+			menu.classList.toggle('open');
+			menu.style.transition = 'max-height 0.3s';
+			
+
+			if (menu.classList.contains('open')) {
+				menu.style.maxHeight = '100%';
+			} else {
+				menu.style.maxHeight = '0';
+			}
 		}
-	  });
-	  parent.classList.add('open');
-	  btnParent.classList.add('hide');
-	  document.addEventListener('mousemove', isMosueLeave);
-	  parent.addEventListener('mouseleave', debounce(parentMoseLeaveHandler));
-	});
-  };
-  
-  const menuMain = () => {
-	const dropdownMenuList = document.querySelectorAll('[data-dropdown-menu="parent"]');
-	if (dropdownMenuList.length) {
-	  dropdownMenuList.forEach((dropdownMenu) => {
-		initDropdownMenu(dropdownMenu);
-	  });
+
+		const hideMenu = () => {
+			btn.classList.remove('open')
+			menu.classList.remove('open')
+			menu.style.maxHeight = '0'
+		}
+
+		btn.addEventListener('click', openMenu)
 	}
-  
   };
-  
-  menuMain();
+
+initMenu(); //вызов функции 
